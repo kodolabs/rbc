6070a5c00584b000593be660dbadc2851d1ed86c
Sergey Pchelincev <jalkoby91@gmail.com>
2012-12-23 01:52:13 +0200

    add custom errors pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rbc::Application.routes.draw do
 
   resources :users, :only => :create
   resources :questionaries, :only => :create
+
+  if Rails.application.config.consider_all_requests_local
+    get "errors/error_404"
+    get "errors/error_500"
+  else
+    match '*not_found', to: 'errors#error_404'
+  end
 end
