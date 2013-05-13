666b8023be04ba91d5d58a21110f2aaae01c5fb2
Alexander Glushkov <cutalion@gmail.com>
2013-05-13 11:58:23 +0300

    Fix modal window and integration spec

--- spec/features/user_save_information_spec.rb
+++ spec/features/user_save_information_spec.rb
@@ -4,30 +4,33 @@ require 'spec_helper'
 feature 'User save information', :js do
   scenario 'submit all forms' do
     visit root_path
-    click_on 'Мне интересно!'
 
+    page.should have_no_selector('#new_questionary', :visible => true)
     page.should have_selector('#new_user', :visible => true)
     within '#new_user' do
       fill_in 'email', :with => 'user@mail.com'
-      click_on 'Мне интересно!'
+      click_on 'Подписаться'
     end
 
     page.should have_selector('#new_questionary', :visible => true)
     within '#new_questionary' do
-      choose Questionary::COURSE_FORMATS.first
-      choose Questionary::YES_NO.first
-      choose Questionary::EXPIRIENCE.first
+      check Questionary::LANGUAGES.first
+      check Questionary::LANGUAGES.last
+      choose Questionary::EXPERIENCE.first
       click_on 'Отправить'
     end
+
+    page.should have_no_selector('#new_questionary', :visible => true)
+    page.should have_no_selector('#new_user', :visible => true)
+    page.should have_selector('#thankyou', :visible => true)
   end
 
   scenario 'submit invalid email' do
     visit root_path
-    click_on 'Мне интересно!'
 
     within '#new_user' do
       fill_in 'email', :with => ''
-      click_on 'Мне интересно!'
+      click_on 'Подписаться'
     end
 
     page.should_not have_selector('#new_questionary', :visible => true)
