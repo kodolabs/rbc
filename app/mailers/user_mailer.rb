316aaca2ce57df3f9c6c0cbd418040723fce8e50
Yury Omelchuk <jurgen@upscript.com>
2013-06-06 10:18:46 +0300

    updated contact email, added phone number

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,17 +1,17 @@
 #encoding: utf-8
 
 class UserMailer < ActionMailer::Base
-  CONTACT = "contact@kodolabs.com"
+  CONTACT = 'support@railskickstart.com.ua'
 
   default from: CONTACT, bcc: CONTACT
 
   def welcome(email)
-    mail to: email, subject: "Railskickstart: будьте в курсе"
+    mail to: email, subject: 'Railskickstart: будьте в курсе'
   end
 
   def registration(reg)
     @name = reg.name
-    mail to: reg.email, subject: "Railskickstart: спасибо за регистрацию"
+    mail to: reg.email, subject: 'Railskickstart: спасибо за регистрацию'
   end
 
 end
