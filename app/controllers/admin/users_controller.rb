ce3bce746a0127664cc1f1c35cf1d40613bdf100
Yury Omelchuk <jurgen@upscript.com>
2013-06-06 10:33:12 +0300

    export users as CSV

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,27 @@
+require 'csv'
+
 class Admin::UsersController < Admin::BaseController
+
+  def index
+    respond_to do |format|
+      format.csv do
+
+        filename = 'subscribers.csv'
+
+        headers["Content-Type"] ||= 'text/csv'
+        headers["Content-Disposition"] = "attachment; filename=\"#{filename}\""
+        headers["Content-Transfer-Encoding"] = "binary"
+
+        csv_string = CSV.generate do |csv|
+          csv << ["Email"]
+          User.all.each do |user|
+            csv << [user.email]
+          end
+        end
+        render :text => csv_string
+      end
+    end
+  end
 
   def destroy
     @user = User.find params[:id]
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -10,4 +10,5 @@ class Registration
 
   validates :name, presence: true
   validates :email, :presence => true, uniqueness: true, format: {:with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i }
+
 end
